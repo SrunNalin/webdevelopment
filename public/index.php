1ab0bc55a09a47535c733f824f34965670a35298
SrunNalin <61485509+SrunNalin@users.noreply.github.com>
2020-03-11 15:24:25 +0700

    cleatr all test

--- public/index.php
+++ public/index.php
@@ -7,56 +7,6 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-var_dump("hello");
-var_dump("hello");
-print_r("cs");
-var_dump("hello");
-print_r("cs");
-var_dump("hello");
-print_r("cs");
-var_dump("hello");
-print_r("cs");
-var_dump("hello");
-print_r("cs");
-var_dump("hello");
-print_r("cs");
-var_dump("hello");
-print_r("cs");
-
-var_dump("Hi");
-var_dump("Hi");
-var_dump("Hi");
-
-var_dump("Hi");
-var_dump("Hi");
-var_dump("Hi");
-
-/*
-    some code here
-    somethings more 
-*/
-
-var_dump("hello world ");
-var_dump("Hi");
-var_dump("Hi");
-
-var_dump("Hi");
-var_dump("Hi");
-var_dump("Hi");
-var_dump("Hi");
-var_dump("Hi");
-var_dump("Hi");
-
-/*
-    some code here
-    somethings more 
-*/
-
-var_dump("hello world ");
-var_dump("Hi");
-var_dump("Hi");
-
-
 
 
 define('LARAVEL_START', microtime(true));
